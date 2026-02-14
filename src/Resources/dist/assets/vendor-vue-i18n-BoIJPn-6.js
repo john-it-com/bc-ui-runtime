a25e1534fe9a491c0acd461567e2c0c5aa79411e
Clemens John <clemens.john@john-it.com>
2026-02-14 21:53:08 +0100

    build: add dist assets and lockfile

---
+++ src/Resources/dist/assets/vendor-vue-i18n-BoIJPn-6.js
@@ -0,0 +1,1 @@
+import{D as n,I as t,a as r,b as I,d as o,N as i,T as m,e as c,c as D,u as N,v as T}from"./vue-i18n-twHPJBzy.js";import"./vue-MlUFOzFR.js";export{n as DatetimeFormat,t as I18nD,r as I18nInjectionKey,I as I18nN,o as I18nT,i as NumberFormat,m as Translation,c as VERSION,D as createI18n,N as useI18n,T as vTDirective};
