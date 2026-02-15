7c6f039d6db6026ffc89f428ccb7658b66cbca17
Clemens John <clemens.john@john-it.com>
2026-02-15 18:09:13 +0100

    build: update dist assets

--- src/Resources/dist/assets/vendor-inertia-CgBX0iw_.js
+++
@@ -1,1 +0,0 @@
-import{Z as r,i as m,j as o,N as t,T as i,Q as p,O as u}from"./inertia-vue3-BpWI2eLp.js";import"./vue-MlUFOzFR.js";export{r as Head,m as Link,o as createInertiaApp,t as router,i as useForm,p as usePage,u as useRemember};
---
+++ src/Resources/dist/assets/vendor-inertia-DkogwV1y.js
@@ -0,0 +1,1 @@
+import{Z as r,i as m,j as o,N as t,T as i,Q as p,O as u}from"./inertia-vue3-C_2XkRmg.js";import"./vue-MlUFOzFR.js";export{r as Head,m as Link,o as createInertiaApp,t as router,i as useForm,p as usePage,u as useRemember};
