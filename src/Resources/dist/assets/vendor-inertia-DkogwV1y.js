5537edcc6ef09796fa14c1261f8fe32508e644a3
Clemens John <clemens.john@john-it.com>
2026-03-04 08:14:51 +0100

    chore: update dist assets

---
+++ src/Resources/dist/assets/vendor-inertia-DkogwV1y.js
@@ -0,0 +1,2 @@
+import{Z as r,i as m,j as o,N as t,T as i,Q as p,O as u}from"./inertia-vue3-C_2XkRmg.js";import"./vue-MlUFOzFR.js";export{r as Head,m as Link,o as createInertiaApp,t as router,i as useForm,p as usePage,u as useRemember};
+//# sourceMappingURL=vendor-inertia-DkogwV1y.js.map
